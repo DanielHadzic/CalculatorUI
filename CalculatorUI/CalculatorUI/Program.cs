ec6618a05a9f9525213c80c88bc05732ed5661d3
Daniel Hadzic <dhadzic@ambrac.nl>
2021-01-13 17:00:21 +0100

    add submit

--- CalculatorUI/CalculatorUI/Program.cs
+++ CalculatorUI/CalculatorUI/Program.cs
@@ -8,6 +8,7 @@ namespace CalculatorUI
         static void Main(string[] args)
         {
             CalculatorEngine.Engine.Add(3, 5);
+            CalculatorEngine.Engine.Submit(5, 3);
             Console.WriteLine("Hello World!");
         }
     }
