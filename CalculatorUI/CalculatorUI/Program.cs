5dd0ccb4fba6b5fce478c25b765aa0dbe26879d2
Daniel Hadzic <dhadzic@ambrac.nl>
2021-01-13 16:01:08 +0100

    add solution and project

---
+++ CalculatorUI/CalculatorUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculatorUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
